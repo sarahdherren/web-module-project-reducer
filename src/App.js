77d36072550ad0d399e1ee88493267b19fa49a01
Sarah D Herren <herren.sarah@icloud.com>
2021-07-21 20:56:11 -0500

    created reducers for all buttons on calculator, linked onClicks for each button to each appropriate dispatch action

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ import CalcButton from './components/CalcButton';
 
 import { initialState } from './reducers/index';
 import reducer from './reducers';
-import { addOne, applyNumber } from './actions/index';
+import { changeOperator, applyNumber, clearDisplay, memorySave, memoryApplied, memoryClear } from './actions/index';
+
 
 function App() {
 
-  const handleClick = (num) => {
+  const handleNum = (num) => {
     console.log(num)
     dispatch(applyNumber(num))
-  }
+  };
+
+  const handleOperator = (operator) => {
+    dispatch(changeOperator(operator))
+  };
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
@@ -36,37 +41,37 @@ function App() {
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton onClick={(e) => dispatch(memorySave())} value={"M+"}/>
+              <CalcButton onClick={(e) => dispatch(memoryApplied())} value={"MR"}/>
+              <CalcButton onClick={(e) => dispatch(memoryClear())} value={"MC"}/>
             </div>
 
             <div className="row">
-              <CalcButton onClick={(e) => handleClick(parseInt(e.target.innerText))} value={1} />
-              <CalcButton onClick={(e) => handleClick(parseInt(e.target.innerText))} value={2}/>
-              <CalcButton onClick={(e) => handleClick(parseInt(e.target.innerText))} value={3}/>
+              <CalcButton onClick={(e) => handleNum(parseInt(e.target.innerText))} value={1} />
+              <CalcButton onClick={(e) => handleNum(parseInt(e.target.innerText))} value={2}/>
+              <CalcButton onClick={(e) => handleNum(parseInt(e.target.innerText))} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton onClick={(e) => handleClick(parseInt(e.target.innerText))} value={4}/>
-              <CalcButton onClick={(e) => handleClick(parseInt(e.target.innerText))} value={5}/>
-              <CalcButton onClick={(e) => handleClick(parseInt(e.target.innerText))} value={6}/>
+              <CalcButton onClick={(e) => handleNum(parseInt(e.target.innerText))} value={4}/>
+              <CalcButton onClick={(e) => handleNum(parseInt(e.target.innerText))} value={5}/>
+              <CalcButton onClick={(e) => handleNum(parseInt(e.target.innerText))} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton onClick={(e) => handleClick(parseInt(e.target.innerText))} value={7}/>
-              <CalcButton onClick={(e) => handleClick(parseInt(e.target.innerText))} value={8}/>
-              <CalcButton onClick={(e) => handleClick(parseInt(e.target.innerText))} value={9}/>
+              <CalcButton onClick={(e) => handleNum(parseInt(e.target.innerText))} value={7}/>
+              <CalcButton onClick={(e) => handleNum(parseInt(e.target.innerText))} value={8}/>
+              <CalcButton onClick={(e) => handleNum(parseInt(e.target.innerText))} value={9}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton onClick={(e) => handleOperator(e.target.innerText)} value={"+"}/>
+              <CalcButton onClick={(e) => handleOperator(e.target.innerText)} value={"*"}/>
+              <CalcButton onClick={(e) => handleOperator(e.target.innerText)} value={"-"}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton onClick={() => dispatch(clearDisplay())} value={"CE"}/>
             </div>
 
           </form>
